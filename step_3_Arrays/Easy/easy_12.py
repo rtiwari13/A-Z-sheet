ceb026de4569429c0937e5f806177f78b919a914
rtiwari13 <ritanshutiwari593@gmail.com>
2025-07-30 10:53:30 +0530

    day 12

--- step_3_Arrays/Easy/easy_12.py
+++ step_3_Arrays/Easy/easy_12.py
@@ -9,9 +9,14 @@
 # Input : nums = [5]
 # Output : 5
 
-# def appeared_once(nums):
-#     count = 0
-#     for i in range (len(nums)-1):
-#         nums[i]
-#         for j in range(len(nums)-1):
-    
+# any number XOR with itself is 0
+# a ^ 0 = a
+# a ^ a = 0
+
+def appeared_once(nums):
+    count = 0
+    for i in nums:     
+            count ^= i
+    return count
+        
+print(appeared_once([1, 2, 2, 4, 3, 1, 4]))
