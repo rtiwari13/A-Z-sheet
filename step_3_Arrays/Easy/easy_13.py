6020c7b7e5c10c3b9c940bada250ef6751a5074c
rtiwari13 <ritanshutiwari593@gmail.com>
2025-07-31 10:05:34 +0530

    day 13: longest sub array

--- step_3_Arrays/Easy/easy_13.py
+++ step_3_Arrays/Easy/easy_13.py
@@ -2,16 +2,24 @@
 #  find the length of the longest sub-array that sums to k.
 #  If no such sub-array exists, return 0.
 
-
-# Examples:
 # Input: nums = [10, 5, 2, 7, 1, 9],  k=15
-
 # Output: 4
 
-# Explanation: The longest sub-array with a sum equal to 15 is [5, 2, 7, 1], which has a length of 4. This sub-array starts at index 1 and ends at index 4, and the sum of its elements (5 + 2 + 7 + 1) equals 15. Therefore, the length of this sub-array is 4.
-
 # Input: nums = [-3, 2, 1], k=6
-
 # Output: 0
 
-# Explanation: There is no sub-array in the array that sums to 6. Therefore, the output is 0.
+# brute force : count sum of each sub array 
+
+def longest_sub_array(nums,k):
+    max_length = 0
+    for i in range(len(nums)):
+        sum = 0
+        for j in range(i , len(nums)):
+            sum += nums[j]
+            
+            if sum == k:
+                max_length = max(max_length,j-i+1)
+    
+    return max_length 
+
+print(longest_sub_array([10, 5, 2, 7, 1, 9],  15))
